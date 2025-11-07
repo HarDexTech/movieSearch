40ea30dff516b7d9727ca472b44029e44aa57fe4
Adesina Ayomide <adesinayomide2@gmail.com>
2025-11-07 13:34:03 +0100

    update files

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 'use strict'
+const searchBtn = document.getElementById('searchButton');
+const movieInput = document.getElementById('searchInput');
+const resultContainer = document.getElementById('results');
