4e4b71317010be4b6cb339f5b8703c9a7738125b
Adesina Ayomide <adesinayomide2@gmail.com>
2025-11-08 06:36:51 +0100

    Added movie search functionality

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 'use strict'
 const searchBtn = document.getElementById('searchButton');
-const movieInput = document.getElementById('searchInput');
 const resultContainer = document.getElementById('results');
+const apikey = "f4c009ac";
+
+const getMovie = function () {
+
+    const movieInput = document.getElementById('searchInput').value;
+    fetch(`http://www.omdbapi.com/?apikey=${apikey}&t=${movieInput}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.Response === "True") {
+                let hasSeasons
+                if (data.Type === "series") {
+                    hasSeasons = `
+                    Type: ${data.Type}<br>
+                    Seasons: ${data.totalSeasons}
+                    `
+                }
+                else {
+                    hasSeasons = `
+                    Type: ${data.Type}
+                    `
+                }
+                resultContainer.innerHTML = `<div class="movieData">
+                Title: ${data.Title}<br>
+                Plot: ${data.Plot}<br>
+                Rating: ${data.imdbRating}<br>
+                Actors: ${data.Actors}<br>
+                Year: ${data.Year}<br>
+                Genre: ${data.Genre}<br>
+                ${hasSeasons}
+                </div>`;
+            }
+            else {
+                resultContainer.innerHTML = `<h2>
+                 Oops!, ${data.Error}
+                </h2>`;
+                console.log(data)
+            }
+        })
+}
