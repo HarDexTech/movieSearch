72031dc79a0a64c7f4219daaf83d502236977ac6
Adesina Ayomide <adesinayomide2@gmail.com>
2025-11-14 22:08:40 +0100

    Refactor: Enhance movie search functionality and add API key configuration

---
+++ config.example.js
@@ -0,0 +1,3 @@
+const APIKEY = {
+  apiKey: "",
+};
--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-'use strict'
-const searchBtn = document.getElementById('searchButton');
-const resultContainer = document.getElementById('results');
-const apikey = "f4c009ac";
+"use strict";
+//dom elements
+const searchBtn = document.getElementById("searchButton");
+const resultContainer = document.getElementById("results");
+const apikey = APIKEY.apiKey;
 
 const getMovie = function () {
-
-    const movieInput = document.getElementById('searchInput').value;
-    if (movieInput.length === 0) {
-        resultContainer.innerHTML = `<h2>
+  const movieInput = document.getElementById("searchInput").value; //get input
+  if (movieInput.length === 0) {
+    resultContainer.innerHTML = `<h2>
         Please enter a movie name
        </h2>`;
-        return;
-    }
-    fetch(`https://www.omdbapi.com/?apikey=${apikey}&t=${movieInput}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.Response === "True") {
-                let hasSeasons
-                if (data.Type === "series") {
-                    hasSeasons = `
+    return; //stop if input is empty
+  }
+
+  // get data from api
+  fetch(`https://www.omdbapi.com/?apikey=${apikey}&t=${movieInput}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.Response === "True") {
+        createOutput(data);
+      } else {
+        resultContainer.innerHTML = `<h2>
+                 Oops!, ${data.Error}
+                </h2>`;
+        console.log(data);
+      }
+    })
+    .catch((error) => {
+      resultContainer.innerHTML = `<h2>
+        Oops!, something went wrong
+       </h2>`;
+      console.log("Error fetching data:", error);
+    });
+};
+
+//enter to search functionality
+function enterSearch(e) {
+  if (e.key === "Enter") {
+    getMovie();
+  }
+}
+document.querySelector("input").addEventListener("keydown", enterSearch);
+
+//build html from data
+function createOutput(data) {
+  let hasSeasons;
+  if (data.Type === "series") {
+    hasSeasons = `
                     Type: ${data.Type}<br>
                     Seasons: ${data.totalSeasons}
-                    `
-                }
-                else {
-                    hasSeasons = `
+                    `;
+  } else {
+    hasSeasons = `
                     Type: ${data.Type}
-                    `
-                }
-                resultContainer.innerHTML = `<div class="movieData">
+                    `;
+  }
+  resultContainer.innerHTML = `<div class="movieData">
                 <div>Title: ${data.Title}</div>
                 <div>Plot: ${data.Plot}</div>
                 <div>Rating: ${data.imdbRating}</div>
@@ -38,20 +65,4 @@ const getMovie = function () {
                 <div>${hasSeasons}</div>
                 <div><img src="${data.Poster}" alt="Movie Poster"></div>             
                 </div>`;
-            }
-            else {
-                resultContainer.innerHTML = `<h2>
-                 Oops!, ${data.Error}
-                </h2>`;
-                console.log(data)
-            }
-        })
-}
-
-document.querySelector('body').addEventListener('keydown', enterSearch);
-
-function enterSearch(e) {
-    if (e.key === 'Enter') {
-        getMovie();
-    }
 }
